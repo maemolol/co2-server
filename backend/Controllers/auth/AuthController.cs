051db8bfcff438e4ec551249538d8358c2919942
Mark Korobkin <markkorobkin85@gmail.com>
2025-10-28 10:49:43 +0200

    added registration endpoint, starting work on login

--- backend/Controllers/auth/AuthController.cs
+++ backend/Controllers/auth/AuthController.cs
@@ -5,6 +5,7 @@ using Validation;
 using Config;
 using Models;
 using Dtos;
+using DevOne.Security.Cryptography.BCrypt;
 
 namespace Controllers;
 
@@ -12,12 +13,54 @@ namespace Controllers;
 [Route("/")]
 public class AuthController : ControllerBase
 {
-    private readonly AppDbContext _context;
+    private readonly AppDbContext context;
     private readonly JwtService _jwt;
 
     public AuthController(AppDbContext context, IConfiguration config)
     {
-        _context = context;
+        this.context = context;
         _jwt = new JwtService(config);
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] UserRegisterDto user)
+    {
+        var validator = new UserRegValidation();
+        var errors = validator.Validate(user);
+
+        if (errors.Any())
+            return BadRequest(new { errors });
+
+        try
+        {
+            if (await context.Users.AnyAsync(u => u.username == user.username))
+                return BadRequest("Username is already registered.");
+
+            var newUser = new Users
+            {
+                user_uid = Guid.NewGuid(),
+                username = user.username,
+                password_hash = BCrypt.Net.BCrypt.HashPassword(user.password)
+            };
+
+            using var transaction = await context.Database.BeginTransactionAsync();
+            await context.Users.AddAsync(newUser);
+            await context.SaveChangesAsync();
+            await transaction.CommitAsync();
+
+            return Ok("User registered successfully");
+        }
+        catch (Exception ex)
+        {
+            return Problem("Registration error: " + ex.Message);
+        }
+    }
+    
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] UserLoginDto user)
+    {
+        var validator = new UserLoginValidation();
+
+        
     }
 }

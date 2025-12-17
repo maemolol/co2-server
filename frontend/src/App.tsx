076852d9fb72bd93610c7d43184ef505f2024e0d
Mark Korobkin <markkorobkin85@gmail.com>
2025-12-17 12:53:58 +0200

    decided to do work on the frontend today

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import type {JSX} from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Footer from "./components/footer/footer";
+import Home from "./pages/home/home";
+
+function ProtectedRoute({ children }: { children: JSX.Element}){
+  const user = localStorage.getItem("user");
+  const isAuth = !!user;
+
+  if(!isAuth) {
+    return <Navigate to="/login" replace />
+  }
+
+  return children;
+}
+
+export function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,9 +1,47 @@
+import {Link} from "react-router-dom";
 import "./home.css";
 
 function Home() {
     return (
         <div class="home">
             <title>CO2 sensor</title>
+            <header className="out-header">
+                <a className="home-link" href="/">CO2 Monitor</a>
+                <div class="header-buttons">
+                    <Link to="/login" className="header-login">Login</Link>
+                    <a href="/login" className="header-login">Login</a>
+                    <a href="/register" className="header-register">Register</a>
+                </div>
+            </header>
+            <main>
+                <section className="home-demo">
+                    <div className="demo-text-button">
+                        <h1 className="demo-title">Monitor your CO2 level in real time</h1>
+                        <p className="demo-subtitle">Dashboard designed for ESP32 with a CO2 sensor attached. See your air quality, both past and present, and get alerts for when you need to ventilate your room.</p>
+                        <div className="demo-buttons">
+                            <a href="/register"><button type="button" className="demo-register">Register</button></a>
+                        </div>
+                    </div>
+                    <div className="demo-showcase">
+                        <p className="demo-dashboard-title">Example dashboard</p>
+                        <div className="demo-co2">CO2 placeholder</div>
+                        <div className="demo-graphs">
+                            <div className="demo-graph">
+                                <p className="demo-graph-title">Current CO2</p>
+                                <p className="demo-graph-subtite">893 ppm</p>
+                            </div>
+                            <div className="demo-graph">
+                                <p className="demo-graph-title">Temperature</p>
+                                <p className="demo-graph-subtite">22.3 °C</p>
+                            </div>
+                            <div className="demo-graph">
+                                <p className="demo-graph-title">Humidity</p>
+                                <p className="demo-graph-subtite">57%</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </main>
         </div>
     );
 }
